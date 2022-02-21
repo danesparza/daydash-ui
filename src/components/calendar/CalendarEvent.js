c22c1e03eee79042d6d217f4beb27ec3e5e30156
Dan Esparza <esparza.dan@gmail.com>
2022-02-21 12:18:37 -0500

    Include 'all day' as a time in the calendar for all day events

--- src/components/calendar/CalendarEvent.js
+++ src/components/calendar/CalendarEvent.js
@@ -2,10 +2,19 @@
 function CalendarEvent(props) {
     //  Format the display
     const summary = props.event.summary;
-    const startTime = new Date(props.event.starttime * 1000).toLocaleTimeString("en-US", {hour12 : true, hour:  "numeric", minute: "numeric"}).toLocaleLowerCase();
-    const endTime = new Date(props.event.endtime * 1000).toLocaleTimeString("en-US", {hour12 : true, hour:  "numeric", minute: "numeric"}).toLocaleLowerCase();
-    let formattedTime = startTime + " - " + endTime;
-    if(startTime === endTime){
+
+    //  Get the start/end dates
+    const startTime =  new Date(props.event.starttime * 1000);
+    const endTime = new Date(props.event.endtime * 1000);
+    
+    //  Format the start/end dates
+    const fmtStartTime = startTime.toLocaleTimeString("en-US", {hour12 : true, hour:  "numeric", minute: "numeric"}).toLocaleLowerCase();
+    const fmtEndTime = endTime.toLocaleTimeString("en-US", {hour12 : true, hour:  "numeric", minute: "numeric"}).toLocaleLowerCase();
+    let formattedTime = fmtStartTime + " - " + fmtEndTime;
+
+    //  If we have an event that lasts > 23 hours, let's just call it "All day"
+    let startEndDiff = Math.abs(endTime.getTime() - startTime.getTime()) / 3600000;
+    if(startEndDiff > 23){
         formattedTime = "All day"; 
     }
 
